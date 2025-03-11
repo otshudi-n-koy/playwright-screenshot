8abe99ce289b730371c25e4cf4453c90fb0c1d34
N'Koy OTSHUDI <nkoy.otshudi@gmail.com>
2025-03-11 21:35:27 +0100

    Configuration pour Chromium uniquement

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,17 +2,13 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30000,
-  expect: {
-    timeout: 5000
-  },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    actionTimeout: 0,
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
 
@@ -20,14 +16,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     },
   ],
 });
